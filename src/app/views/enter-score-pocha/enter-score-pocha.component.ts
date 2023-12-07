7cbdd3eb4702598c0afed4792f707ef5a5d6b660
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-12-07 19:54:49 +0100

    Logic on click keyboard

--- src/app/views/enter-score-pocha/enter-score-pocha.component.ts
+++ src/app/views/enter-score-pocha/enter-score-pocha.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LOCAL_STORE_KEYS } from '../../constants/local-storage-keys';
 import { GameHolderService } from '../../game-services/game-holder.service';
 import { Player } from '../../interfaces/player';
 import { enterScorePochaAnimation } from './enter-score-pocha.animation';
@@ -20,6 +21,14 @@ export class EnterScorePochaComponent {
 
   public currentPlayerIndex = 0;
 
+  private get puntuationCurrentPlayer() {
+    return this.players[this.currentPlayerIndex].punctuation;
+  }
+
+  private set puntuationCurrentPlayer(punctuation: number) {
+    this.players[this.currentPlayerIndex].punctuation = punctuation;
+  }
+
   public constructor(private readonly router: Router, private readonly location: Location, private readonly gameHolderService: GameHolderService) {
     this.roundNumber = this.router.getCurrentNavigation()?.extras?.state?.['roundNumber'];
     this.players = this.router.getCurrentNavigation()?.extras?.state?.['players'];
@@ -29,17 +38,62 @@ export class EnterScorePochaComponent {
     this.location.back();
   }
 
-  public nextPlayer() {
-    if (this.currentPlayerIndex === this.players.length - 1) {
-      // TODO it is last player to enter score, save and return
+  public onClickKeyboard(event: Event) {
+    const buttonKey = (event.target as HTMLElement).closest('button')?.textContent;
 
+    if (buttonKey == null) {
+      // clicked the keyboard, but not any button
       return;
     }
 
-    this.currentPlayerIndex++;
+    if (buttonKey === '→' || buttonKey === '✔️') {
+      this.currentPlayerIndex++;
+      if (this.currentPlayerIndex === this.players.length) {
+        this.finishEnterScore();
+      }
+      return;
+    }
+
+    if (buttonKey === '←' && this.currentPlayerIndex > 0) {
+      this.currentPlayerIndex--;
+      return;
+    }
+
+    if (buttonKey === 'restar10') {
+      this.puntuationCurrentPlayer -= 10;
+      return;
+    }
+
+    if (buttonKey === 'sumar10') {
+      this.puntuationCurrentPlayer += 10;
+      return;
+    }
+
+    this.puntuationCurrentPlayer = +buttonKey;
   }
 
-  public onClickKeyboard(event: any) {
-    // TODO set player punctuation
+  public finishEnterScore() {
+    // change the player that deals only if it is a new round (not edition in table view)
+    if (this.roundNumber === this.gameHolderService.service.getNextRoundNumber()) {
+      this.gameHolderService.service.setNextDealingPlayer();
+    }
+    this.players.forEach((p) => (p.scores[this.roundNumber - 1] = p.punctuation));
+    this.saveGameLocalStorage();
+    this.location.back();
+  }
+
+  private saveGameLocalStorage() {
+    localStorage.setItem(LOCAL_STORE_KEYS.PLAYERS, JSON.stringify(this.gameHolderService.service.players));
+    localStorage.setItem(
+      LOCAL_STORE_KEYS.CONFIG,
+      JSON.stringify({
+        numberOfCards: this.gameHolderService.service.numberOfCards,
+        limitScore: this.gameHolderService.service.limitScore,
+        winner: this.gameHolderService.service.winner,
+      })
+    );
+    localStorage.setItem(LOCAL_STORE_KEYS.DEALING_PLAYER_INDEX, JSON.stringify(this.gameHolderService.service.dealingPlayerIndex));
+    localStorage.setItem(LOCAL_STORE_KEYS.GAME_NAME, this.gameHolderService.service.gameName);
+    localStorage.setItem(LOCAL_STORE_KEYS.TIME_LAST_GAME, JSON.stringify(Date.now()));
   }
 }
