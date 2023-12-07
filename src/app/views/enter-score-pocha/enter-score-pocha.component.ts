384331f71141a547019550926a6810e2165174f5
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-12-07 18:01:41 +0100

    Navigation back logic and change player animation

--- src/app/views/enter-score-pocha/enter-score-pocha.component.ts
+++ src/app/views/enter-score-pocha/enter-score-pocha.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GameHolderService } from '../../game-services/game-holder.service';
+import { Player } from '../../interfaces/player';
+import { enterScorePochaAnimation } from './enter-score-pocha.animation';
 
 @Component({
   selector: 'app-enter-score-pocha',
+  standalone: true,
+  imports: [CommonModule],
+  providers: [Location],
   templateUrl: './enter-score-pocha.component.html',
   styleUrls: ['./enter-score-pocha.component.scss'],
+  animations: [enterScorePochaAnimation],
 })
-export class EnterScorePochaComponent implements OnInit {
-  public constructor() {}
+export class EnterScorePochaComponent {
+  public players: Player[];
+  public roundNumber: number;
 
-  public ngOnInit() {}
+  public currentPlayerIndex = 0;
+
+  public constructor(private readonly router: Router, private readonly location: Location, private readonly gameHolderService: GameHolderService) {
+    this.roundNumber = this.router.getCurrentNavigation()?.extras?.state?.['roundNumber'];
+    this.players = this.router.getCurrentNavigation()?.extras?.state?.['players'];
+  }
+
+  public closeEnterScorePocha() {
+    this.location.back();
+  }
 }
