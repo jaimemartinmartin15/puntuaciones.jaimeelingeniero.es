1156b22fd9614bc0dba2b189029cd0acb09864a3
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-12-20 15:35:23 +0100

    Unit test round info chinchon game

--- src/app/components/round-info/round-info.component.spec.ts
+++ src/app/components/round-info/round-info.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { ChinchonService } from '../../game-services/chinchon.service';
 import { GameHolderService } from '../../game-services/game-holder.service';
 import { PochaService } from '../../game-services/pocha.service';
 import { provideGameService } from '../../game-services/utils';
@@ -91,6 +92,77 @@ describe('RoundInfoComponent', () => {
       gameHolderService.service.players = [
         { id: 0, name: 'Player 1', scores: [-10, -10, 20, -10, -10], punctuation: 0 },
         { id: 1, name: 'Player 2', scores: [5, 10, -10, 5, 5], punctuation: 0 },
+      ];
+      fixture.detectChanges();
+
+      const gameHasFinished = fixture.debugElement.query(By.css(SELECTORS.GAME_HAS_FINISHED)).nativeElement;
+      expect(gameHasFinished.textContent.length).toBeGreaterThan(5); // whatever number, just check it is not empty
+    });
+  });
+
+  describe('Chinchón game', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [RoundInfoComponent],
+        providers: [
+          provideRouter([]),
+          { provide: ComponentFixtureAutoDetect, useValue: true },
+          { provide: GameHolderService, useClass: GameHolderService },
+          provideGameService(ChinchonService),
+        ],
+      });
+      gameHolderService = TestBed.inject(GameHolderService);
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [], punctuation: 0 },
+      ];
+      fixture = TestBed.createComponent(RoundInfoComponent);
+    });
+
+    it('should show the game name', () => {
+      const gameName = fixture.debugElement.query(By.css(SELECTORS.GAME_NAME)).nativeElement;
+      expect(gameName.textContent).toEqual('Chinchón');
+    });
+
+    it('should show the next round number', () => {
+      const nextRoundNumber = fixture.debugElement.query(By.css(SELECTORS.NEXT_ROUND_NUMBER)).nativeElement;
+      expect(nextRoundNumber.textContent).toContain('Ronda: 1');
+
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [23], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [7], punctuation: 0 },
+      ];
+      fixture.detectChanges();
+      expect(nextRoundNumber.textContent).toContain('Ronda: 2');
+    });
+
+    it('should show the player name that deals', () => {
+      const playerNameThatDeals = fixture.debugElement.query(By.css(SELECTORS.PLAYER_NAME_THAT_DEALS)).nativeElement;
+      expect(playerNameThatDeals.textContent).toContain('Reparte: Player 1');
+
+      gameHolderService.service.dealingPlayerIndex = 1;
+      fixture.detectChanges();
+      expect(playerNameThatDeals.textContent).toContain('Reparte: Player 2');
+    });
+
+    it('should not show the number of cards to deal in next round', () => {
+      const numberOfCardsToDeal = fixture.debugElement.query(By.css(SELECTORS.NUMBER_OF_CARDS_TO_DEAL_NEXT_ROUND));
+      expect(numberOfCardsToDeal).toBeNull();
+    });
+
+    it('should show the limit score', () => {
+      const limitScore = fixture.debugElement.query(By.css(SELECTORS.LIMINT_SCORE)).nativeElement;
+      expect(limitScore.textContent).toContain('Límite: 100');
+
+      gameHolderService.service.limitScore = 60;
+      fixture.detectChanges();
+      expect(limitScore.textContent).toContain('Límite: 60');
+    });
+
+    it('should show if the game has finished', () => {
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [70, 35], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [75, 8], punctuation: 0 },
       ];
       fixture.detectChanges();
 
