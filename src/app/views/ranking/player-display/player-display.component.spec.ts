002330dbd17a076e874c58bedb272780bf72b78b
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-12-20 12:11:33 +0100

    Unit test player display for chinchon game

--- src/app/views/ranking/player-display/player-display.component.spec.ts
+++ src/app/views/ranking/player-display/player-display.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { ChinchonService } from '../../../game-services/chinchon.service';
 import { GameHolderService } from '../../../game-services/game-holder.service';
 import { PochaService } from '../../../game-services/pocha.service';
 import { provideGameService } from '../../../game-services/utils';
@@ -62,6 +63,20 @@ describe('PlayerDisplayComponent', () => {
       expect(fixture.nativeElement).toHaveClass('position-4');
     });
 
+    it('should show the player name and total score', () => {
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [-10, -10], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [5, 5], punctuation: 0 },
+        { id: 2, name: 'Player 3', scores: [10, 5], punctuation: 0 },
+        { id: 3, name: 'Player 4', scores: [5, -10], punctuation: 0 },
+      ];
+      fixture = TestBed.createComponent(PlayerDisplayComponent);
+
+      fixture.componentInstance.playerId = 3;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.PLAYER_NAME_AND_TOTAL_SCORE)).nativeElement.innerText).toContain('Player 4\n-5');
+    });
+
     it('should show the last round score', () => {
       gameHolderService.service.players = [
         { id: 0, name: 'Player 1', scores: [5, 10], punctuation: 0 },
@@ -98,18 +113,113 @@ describe('PlayerDisplayComponent', () => {
       expect(fixture.debugElement.query(By.css(SELECTORS.MAXIMUM_REACHED_SCORE)).nativeElement.innerText).toContain('Máximo alcanzado: 10');
     });
 
-    it('should show the player name and total score', () => {
+    it('should not show number of rejoins', () => {
+      gameHolderService.service.players = [{ id: 0, name: 'Player 1', scores: [5, 10], punctuation: 0 }];
+      fixture = TestBed.createComponent(PlayerDisplayComponent);
+
+      fixture.componentInstance.playerId = 0;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.NUMBER_OF_REJOINS))).toBeNull();
+    });
+  });
+
+  describe('Chinchón game', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [PlayerDisplayComponent],
+        providers: [
+          provideRouter([]),
+          { provide: ComponentFixtureAutoDetect, useValue: true },
+          { provide: GameHolderService, useClass: GameHolderService },
+          provideGameService(ChinchonService),
+        ],
+      });
+      gameHolderService = TestBed.inject(GameHolderService);
+    });
+
+    it('should show the position number and medal with proper css class', () => {
       gameHolderService.service.players = [
-        { id: 0, name: 'Player 1', scores: [-10, -10], punctuation: 0 },
-        { id: 1, name: 'Player 2', scores: [5, 5], punctuation: 0 },
-        { id: 2, name: 'Player 3', scores: [10, 5], punctuation: 0 },
-        { id: 3, name: 'Player 4', scores: [5, -10], punctuation: 0 },
+        { id: 0, name: 'Player 1', scores: [2], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [15], punctuation: 0 },
+        { id: 2, name: 'Player 3', scores: [7], punctuation: 0 },
+        { id: 3, name: 'Player 4', scores: [15], punctuation: 0 },
       ];
       fixture = TestBed.createComponent(PlayerDisplayComponent);
 
+      fixture.componentInstance.playerId = 0;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.POSITION)).nativeElement.innerText).toContain('1\n🥇');
+      expect(fixture.nativeElement).toHaveClass('position-1');
+
+      fixture.componentInstance.playerId = 2;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.POSITION)).nativeElement.innerText).toContain('2\n🥈');
+      expect(fixture.nativeElement).toHaveClass('position-2');
+
+      fixture.componentInstance.playerId = 1;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.POSITION)).nativeElement.innerText).toContain('3\n🥉');
+      expect(fixture.nativeElement).toHaveClass('position-3');
+
       fixture.componentInstance.playerId = 3;
       fixture.detectChanges();
-      expect(fixture.debugElement.query(By.css(SELECTORS.PLAYER_NAME_AND_TOTAL_SCORE)).nativeElement.innerText).toContain('Player 4\n-5');
+      expect(fixture.debugElement.query(By.css(SELECTORS.POSITION)).nativeElement.innerText).toContain('3\n🥉');
+      expect(fixture.nativeElement).toHaveClass('position-3');
+    });
+
+    it('should show the player name and total score', () => {
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [1, 1, 1], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [2, 1, 10], punctuation: 0 },
+        { id: 2, name: 'Player 3', scores: [3, 6, 1], punctuation: 0 },
+        { id: 3, name: 'Player 4', scores: [12, 8, 2], punctuation: 0 },
+      ];
+      gameHolderService.service.limitScore = 10;
+      fixture = TestBed.createComponent(PlayerDisplayComponent);
+
+      fixture.componentInstance.playerId = 0;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.PLAYER_NAME_AND_TOTAL_SCORE)).nativeElement.innerText).toContain('Player 1\n3');
+
+      fixture.componentInstance.playerId = 1;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.PLAYER_NAME_AND_TOTAL_SCORE)).nativeElement.innerText).toContain('Player 2\n10');
+    });
+
+    it('should not show maximum reached score', () => {
+      gameHolderService.service.players = [{ id: 0, name: 'Player 1', scores: [5, 10], punctuation: 0 }];
+      fixture = TestBed.createComponent(PlayerDisplayComponent);
+
+      fixture.componentInstance.playerId = 0;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.MAXIMUM_REACHED_SCORE))).toBeNull();
+    });
+
+    it('should show the number of rejoins', () => {
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [1, 1, 1], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [2, 1, 10], punctuation: 0 },
+        { id: 2, name: 'Player 3', scores: [3, 6, 1], punctuation: 0 },
+        { id: 3, name: 'Player 4', scores: [12, 8, 2], punctuation: 0 },
+      ];
+      gameHolderService.service.limitScore = 10;
+      fixture = TestBed.createComponent(PlayerDisplayComponent);
+
+      fixture.componentInstance.playerId = 0;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.NUMBER_OF_REJOINS)).nativeElement.innerText).toContain('Reenganches: 0');
+
+      fixture.componentInstance.playerId = 2;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.NUMBER_OF_REJOINS)).nativeElement.innerText).toContain('Reenganches: 0');
+
+      fixture.componentInstance.playerId = 1;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.NUMBER_OF_REJOINS)).nativeElement.innerText).toContain('Reenganches: 1');
+
+      fixture.componentInstance.playerId = 3;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css(SELECTORS.NUMBER_OF_REJOINS)).nativeElement.innerText).toContain('Reenganches: 3');
     });
   });
 });
