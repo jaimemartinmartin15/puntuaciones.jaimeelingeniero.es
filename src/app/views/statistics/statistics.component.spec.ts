1d16589a2e15109652be07f3ddb03fa4302cc6cd
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-12-21 20:21:34 +0100

    Unit test statistics other game

--- src/app/views/statistics/statistics.component.spec.ts
+++ src/app/views/statistics/statistics.component.spec.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { LOCAL_STORE_KEYS } from '../../constants/local-storage-keys';
 import { ChinchonService } from '../../game-services/chinchon.service';
 import { GameHolderService } from '../../game-services/game-holder.service';
+import { OtherGameService } from '../../game-services/other-game.service';
 import { PochaService } from '../../game-services/pocha.service';
 import { provideGameService } from '../../game-services/utils';
 import { StatisticsComponent } from './statistics.component';
@@ -130,6 +131,62 @@ describe('StatisticsComponent', () => {
     it('should show played time', () => {
       const playedTime = fixture.debugElement.query(By.css(SELECTORS.PLAYER_TIME)).nativeElement;
       expect(playedTime.textContent).toContain('7 minutos');
+    });
+  });
+
+  describe('Other game', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [StatisticsComponent],
+        providers: [
+          provideRouter([]),
+          { provide: ComponentFixtureAutoDetect, useValue: true },
+          { provide: GameHolderService, useClass: GameHolderService },
+          provideGameService(OtherGameService),
+        ],
+      });
+      gameHolderService = TestBed.inject(GameHolderService);
+      gameHolderService.service.players = [
+        { id: 0, name: 'Player 1', scores: [12, 23, 56], punctuation: 0 },
+        { id: 1, name: 'Player 2', scores: [2, 4, 42], punctuation: 0 },
+        { id: 2, name: 'Player 3', scores: [42, 12, 2], punctuation: 0 },
+      ];
+      localStorage.setItem(LOCAL_STORE_KEYS.TIME_GAME_STARTS, JSON.stringify(Date.now() - 3660000));
+      fixture = TestBed.createComponent(StatisticsComponent);
+      component = fixture.componentInstance;
+    });
+
+    it('should show message when game has no started', () => {
+      gameHolderService.service.players = [{ id: 0, name: 'Player 1', scores: [], punctuation: 0 }];
+      fixture.detectChanges();
+
+      const emptyMessage = fixture.debugElement.query(By.css('.empty-state')).nativeElement.textContent;
+      expect(emptyMessage).toContain('Introduce al menos una ronda para mostrar las estadísticas');
+    });
+
+    it('should show players in first position', () => {
+      const playersInFirstPosition = fixture.debugElement.query(By.css(SELECTORS.PLAYERS_IN_FIRST_POSITION)).nativeElement;
+      expect(playersInFirstPosition.textContent).toEqual('Player 1');
+    });
+
+    it('should show players in last position', () => {
+      const playersInLastPosition = fixture.debugElement.query(By.css(SELECTORS.PLAYERS_IN_LAST_POSITION)).nativeElement;
+      expect(playersInLastPosition.textContent).toEqual('Player 2');
+    });
+
+    it('should show maximum score in one round', () => {
+      const maximumScoreInOneRound = fixture.debugElement.query(By.css(SELECTORS.PLAYERS_MAXIMUM_SCORE_IN_ONE_ROUND)).nativeElement;
+      expect(maximumScoreInOneRound.textContent).toContain('56 por Player 1');
+    });
+
+    it('should show minimum score in one round', () => {
+      const minimumScoreInOneRound = fixture.debugElement.query(By.css(SELECTORS.PLAYERS_MINIMUM_SCORE_IN_ONE_ROUND)).nativeElement;
+      expect(minimumScoreInOneRound.textContent).toContain('2 por Player 2 y Player 3');
+    });
+
+    it('should show played time', () => {
+      const playedTime = fixture.debugElement.query(By.css(SELECTORS.PLAYER_TIME)).nativeElement;
+      expect(playedTime.textContent).toContain('1 hora y 1 minuto');
     });
   });
 });
