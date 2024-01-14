b60d427127a46b11d8ef9abf829e94ceab2586ba
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-01-14 18:47:13 +0100

    Infinite scroll and scroll snap simulation to select digits

--- src/app/components/number-scroller/number-scroller.component.ts
+++ src/app/components/number-scroller/number-scroller.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Input, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, QueryList, ViewChildren } from '@angular/core';
+
+// like in scss file
+const CONSTANTS = {
+  HOST_ELEMENT_HEIGHT: 109,
+  GAP_BETWEEN_DIGITS: 12,
+  DIGIT_ELEMENT_HEIGHT: 25,
+};
 
 @Component({
   selector: 'app-number-scroller',
@@ -15,29 +22,78 @@ export class NumberScrollerComponent implements AfterViewInit {
   @Input()
   public digitsOfTheNumber = [0, 0];
 
+  public mouseDragInfo: { isMouseDown: boolean; digitScrollerEl: HTMLDivElement | null; top: number; y: number } = {
+    isMouseDown: false,
+    digitScrollerEl: null,
+    top: CONSTANTS.DIGIT_ELEMENT_HEIGHT + CONSTANTS.GAP_BETWEEN_DIGITS / 2, // current scroll
+    y: 0, // current mouse position
+  };
+
   public ngAfterViewInit(): void {
     // center the scroll
-    this.digitScrollableElements.map((d) => d.nativeElement.scroll({ top: 25 }));
+    this.digitScrollableElements.map((d) => d.nativeElement.scroll({ top: CONSTANTS.DIGIT_ELEMENT_HEIGHT + CONSTANTS.GAP_BETWEEN_DIGITS / 2 }));
   }
 
-  public onScrollDigit(i: number) {
-    const digitScrollableEl = this.digitScrollableElements.toArray()[i].nativeElement;
+  private getPositionEventY(event: MouseEvent | TouchEvent): number {
+    return (event as MouseEvent).clientY ?? (event as TouchEvent).touches[0].clientY;
+  }
 
-    const scrollOffset = digitScrollableEl.scrollTop;
+  @HostListener('mousedown', ['$event'])
+  @HostListener('touchstart', ['$event'])
+  public onMouseDown(event: MouseEvent | TouchEvent) {
+    const digitScroller = (event.target as Element).closest('.digitScroller') as HTMLDivElement;
+    this.mouseDragInfo.digitScrollerEl = digitScroller;
+    this.mouseDragInfo.isMouseDown = true;
+    this.mouseDragInfo.top = digitScroller?.scrollTop ?? 0;
+    this.mouseDragInfo.y = this.getPositionEventY(event);
+  }
 
-    if (scrollOffset === 0) {
-      this.digitsOfTheNumber[i]--;
-      if (this.digitsOfTheNumber[i] < 0) {
-        this.digitsOfTheNumber[i] = 9;
+  @HostListener('document:mousemove', ['$event'])
+  @HostListener('document:touchmove', ['$event'])
+  public onMouseMove(event: MouseEvent | TouchEvent) {
+    if (this.mouseDragInfo.isMouseDown && this.mouseDragInfo.digitScrollerEl !== null) {
+      // How far the mouse has been moved
+      const mouseMoveY = this.getPositionEventY(event) - this.mouseDragInfo.y;
+
+      const digitIndex = this.digitScrollableElements
+        .toArray()
+        .map((e) => e.nativeElement)
+        .indexOf(this.mouseDragInfo.digitScrollerEl);
+
+      if (this.mouseDragInfo.digitScrollerEl.scrollTop < CONSTANTS.DIGIT_ELEMENT_HEIGHT / 2) {
+        this.digitsOfTheNumber[digitIndex]--;
+        if (this.digitsOfTheNumber[digitIndex] < 0) {
+          this.digitsOfTheNumber[digitIndex] = 9;
+        }
+        this.mouseDragInfo.digitScrollerEl.scrollTo({ top: CONSTANTS.DIGIT_ELEMENT_HEIGHT * 1.5 + CONSTANTS.GAP_BETWEEN_DIGITS / 2 });
+        this.mouseDragInfo.top = CONSTANTS.DIGIT_ELEMENT_HEIGHT * 1.5 + CONSTANTS.GAP_BETWEEN_DIGITS / 2;
+        this.mouseDragInfo.y = this.getPositionEventY(event);
+        return;
+      } else if (this.mouseDragInfo.digitScrollerEl.scrollTop > CONSTANTS.DIGIT_ELEMENT_HEIGHT * 1.5 + CONSTANTS.GAP_BETWEEN_DIGITS) {
+        this.digitsOfTheNumber[digitIndex]++;
+        if (this.digitsOfTheNumber[digitIndex] > 9) {
+          this.digitsOfTheNumber[digitIndex] = 0;
+        }
+        this.mouseDragInfo.digitScrollerEl.scrollTo({ top: CONSTANTS.DIGIT_ELEMENT_HEIGHT / 2 + CONSTANTS.GAP_BETWEEN_DIGITS / 2 });
+        this.mouseDragInfo.top = CONSTANTS.DIGIT_ELEMENT_HEIGHT / 2 + CONSTANTS.GAP_BETWEEN_DIGITS / 2;
+        this.mouseDragInfo.y = this.getPositionEventY(event);
+        return;
       }
-      digitScrollableEl.scroll({ top: 25 });
-    } else if (scrollOffset > 50) {
-      this.digitsOfTheNumber[i]++;
-      if (this.digitsOfTheNumber[i] > 9) {
-        this.digitsOfTheNumber[i] = 0;
-      }
-      digitScrollableEl.scroll({ top: 25 });
+
+      // scroll the element
+      this.mouseDragInfo.digitScrollerEl.scrollTop = this.mouseDragInfo.top - mouseMoveY;
     }
+  }
+
+  @HostListener('document:mouseup')
+  @HostListener('document:touchend')
+  public onMouseUp() {
+    if (this.mouseDragInfo.digitScrollerEl !== null) {
+      this.mouseDragInfo.digitScrollerEl.scrollTo({ top: CONSTANTS.DIGIT_ELEMENT_HEIGHT + CONSTANTS.GAP_BETWEEN_DIGITS / 2 });
+    }
+
+    this.mouseDragInfo.isMouseDown = false;
+    this.mouseDragInfo.digitScrollerEl = null;
   }
 
   public trackByDigit(i: number) {
