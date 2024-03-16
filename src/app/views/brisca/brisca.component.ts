af0acd0baf06d96673be6619449763b3d4db0bdc
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-03-16 17:42:24 +0100

    Create lines of random size and position

--- src/app/views/brisca/brisca.component.ts
+++ src/app/views/brisca/brisca.component.ts
@@ -6,11 +6,6 @@ import { BriscaService } from '../../game-services/brisca.service';
 import { GameHolderService } from '../../game-services/game-holder.service';
 import { intervalArray } from '../../utils/arrays';
 
-interface Row {
-  name: string;
-  score: number;
-}
-
 @Component({
   selector: 'app-brisca',
   standalone: true,
@@ -19,10 +14,18 @@ interface Row {
   styleUrls: ['./brisca.component.scss'],
 })
 export class BriscaComponent implements OnInit {
+  public readonly HORIZONTAL_SEPARATOR_HEIGHT = 12;
   public readonly GAP_STRIPE = 7;
   public readonly BULLET_INTERVAL = 5;
+
   public svgHeight: number;
-  public rows: Row[][] = [];
+
+  public pairNames: string[][] = [];
+  public pairScores: number[][] = [];
+  // pair - left or right - stripe index
+  public svgPairStripes: string[][][] = [];
+  // pair - left or right - bullet index - x y of circle and text
+  public svgBulletPairs: number[][][][] = [];
 
   public briscaService: BriscaService;
 
@@ -42,22 +45,43 @@ export class BriscaComponent implements OnInit {
       names = this.briscaService.teamNames;
     }
 
+    // generate pairs for names (header)
     for (let i = 0; i < names.length; i += 2) {
-      this.rows.push(names.slice(i, i + 2).map((name, i2) => ({ name, score: this.briscaService.scores[i + i2] })));
+      this.pairNames.push([names[i], names[i + 1]]);
+    }
+
+    // generate pairs for scores (header)
+    for (let i = 0; i < names.length; i += 2) {
+      this.pairScores.push([this.briscaService.scores[i], this.briscaService.scores[i + 1]]);
+    }
+
+    // generate pairs for stripes and bullets
+    for (let i = 0; i < names.length; i += 2) {
+      this.svgBulletPairs.push([[], []]);
+      this.svgPairStripes.push([this.getSvgStripesForNameIndex(i), this.getSvgStripesForNameIndex(i + 1)]);
     }
   }
 
-  public getViewBoxForRow(row: Row[]): string {
-    this.svgHeight = 40 + Math.max(row[0].score, row[1]?.score ?? 0) * this.GAP_STRIPE;
+  private getSvgStripesForNameIndex(i: number): string[] {
+    const highStart = 12;
+
+    return intervalArray(this.briscaService.scores[i]).map((stripeN) => {
+      const stripeLenght = (i % 2 === 0 ? 20 : 80) + Math.floor(Math.random() * 5) - 2; // -2 ... 2
+      const gapStripeOffset = Math.floor(Math.random() * 3) - 1; // -1 ... 1
+      const stripeHigh = highStart + stripeN * this.GAP_STRIPE + gapStripeOffset;
+
+      // generate a bullet for the stripe
+      if (stripeN % this.BULLET_INTERVAL === 0) {
+        this.svgBulletPairs[Math.floor(i / 2)][i % 2].push([stripeLenght, stripeHigh]);
+      }
+
+      return `M 50,${stripeHigh} ${stripeLenght},${stripeHigh}`;
+    });
+  }
+
+  public getViewBoxForSvgPair(i: number): string {
+    const minimumVerticalSeparatorHeight = this.HORIZONTAL_SEPARATOR_HEIGHT + 20;
+    this.svgHeight = minimumVerticalSeparatorHeight + Math.max(this.pairScores[i][0], this.pairScores[i][1] ?? 0) * this.GAP_STRIPE;
     return `0 0 100 ${this.svgHeight}`;
-  }
-
-  public arrayOf(n: number): number[] {
-    return intervalArray(n);
-  }
-
-  public random(): number {
-    // -1, 0 or +1
-    return Math.floor(Math.random() * 3) - 1;
   }
 }
