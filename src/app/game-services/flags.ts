7a77ca20b8b281da7d2aab443e7d43d7ada3ee19
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-04-28 13:04:15 +0200

    Refactor of game service and flags. Delete player interface and enter score base

--- src/app/game-services/flags.ts
+++ src/app/game-services/flags.ts
@@ -1,14 +1,30 @@
-import { FormControl } from '@angular/forms';
+import { FormArray, FormControl } from '@angular/forms';
+import { EnterPlayerNamesModel } from '../components/enter-player-names/enter-player-names.component';
 import { RoutingPath } from '../constants/routes';
-import { Player } from '../interfaces/player';
 
 // Note: some flags might have duplicated fields, but because it is required for both
 
 export type FlagMapping = {
+  // general for the game
+  'game:gameStartEnd': { gameHasStarted: () => boolean; gameHasFinished: () => boolean };
+  'game:localStorageSave': {
+    saveStateToLocalStorage(): void;
+    loadStateFromLocalStorage(): void;
+  };
+  'game:rounds': {
+    getNextRoundNumber: () => number;
+  };
+
   // resume game
   'resumeGame:gameName': { gameName: string };
 
   // game config
+  gameConfig: {
+    onStartGame(): void;
+    onEditConfigCurrentGame(): void;
+  };
+  'gameConfig:validation': { isGameConfigCorrect(): boolean };
+  'gameConfig:players': { allowEditTeamName: boolean[]; teamControls: FormArray<FormControl<EnterPlayerNamesModel>> };
   'gameConfig:numberOfCards': { numberOfCards: number; numberOfCardsFormControl: FormControl<number> };
   'gameConfig:limitScore': { limitScore: number; limitScoreFormControl: FormControl<number>; numberOfScrollers: number };
   'gameConfig:winner': { winner: 'highestScore' | 'lowestScore'; winnerFormControl: FormControl<'highestScore' | 'lowestScore'> };
@@ -16,6 +32,7 @@ export type FlagMapping = {
 
   // round info
   'roundInfo:gameName': { gameName: string };
+  'roundInfo:dealingPlayer': { getPlayerNameThatDeals: () => string };
   'roundInfo:numberOfCards': { getNumberOfCardsToDealNextRound: () => string };
   'roundInfo:limitScore': { limitScore: number };
 
@@ -26,24 +43,32 @@ export type FlagMapping = {
 
   // raking view
   ranking: {
-    getRankingPlayers(round?: number): Player[];
-    // required for player display
-    getPlayerPosition: (playerId: number) => number;
-    getScoreLastRound: (playerId: number) => number;
-    getPlayerName: (playerId: number) => string;
-    getTotalScore: (playerId: number) => number;
+    getRankingPlayers(round?: number): number[];
   };
 
   // player display in ranking view
+  'ranking:playerDisplay': {
+    getPlayerPosition: (playerId: number) => number;
+    getPlayerName: (playerId: number) => string;
+    getTotalScore: (playerId: number) => number;
+    getScoreLastRound: (playerId: number) => number;
+    enterScoreRoute: RoutingPath;
+  };
+
+  // player display in ranking view (optionals)
   'ranking:playerDisplay:maximumReachedScore': { getMaximumReachedScore: (playerId: number) => number };
   'ranking:playerDisplay:numberOfRejoins': { getNumberOfRejoins: (playerId: number) => number };
 
   // scoreboard view
   scoreboard: {
-    getPlayerAccumulatedScoreAtRound: (playerId: number, round: number) => number;
+    enterScoreRoute: RoutingPath;
+    playerNames: string[];
+    getPlayerScore: (playerId: number, round?: number) => number;
+    getTotalScore: (playerId: number, round?: number) => number;
     getCellBackgroundColor: (score: number) => string;
-    getTotalScore: (playerId: number) => number;
   };
+
+  // scoreboard view (optionals)
   'scoreboard:specialRounds': {
     showSpecialRowAfterRound: (round: number) => boolean;
     getSpecialRoundScores: (round: number) => number[];
@@ -52,7 +77,6 @@ export type FlagMapping = {
 
   // statistics view
   statistics: {
-    showProgressGraph: boolean;
     getPlayersInFirstPosition: () => string;
     getPlayersInLastPosition: () => string;
     getMaximumScoreInOneRound: () => number;
@@ -60,17 +84,55 @@ export type FlagMapping = {
     getMinimumScoreInOneRound: () => number;
     getPlayerNamesWithMinimumScoreInOneRound: () => string;
   };
+
+  // statistics view (optionals)
   'statistics:progressGraph': {
     svgWidth: number;
     svgHeight: number;
+    getSvgPlayerLine: (playerId: number) => string;
     svgXAxisHeight: number;
-    getSvgPlayerLine: (player: Player) => string;
+    playerNames: string[];
+    getNextRoundNumber: () => number;
+    getRankingPlayers(round: number): number[];
+    getPlayerName: (playerId: number) => string;
     getTotalScore: (playerId: number, round: number) => number;
-    getRankingPlayers(round: number): Player[];
     getPlayerPosition(playerId: number, round: number): number;
   };
+
+  // statistics view progress graph (optionals)
   'statistics:progressGraph:limitScore': {
     svgLimitScoreHeight: number;
+  };
+
+  // brisca view
+  brisca: {
+    modality: 'individual' | 'teams';
+    playerNames: string[];
+    teamNames: string[];
+    scores: number[];
+    setPreviousDealingPlayerIndex: () => void;
+  };
+
+  // enter score view
+  enterScore: {
+    getNextRoundNumber: () => number;
+    setNextDealingPlayer(): void;
+    getPlayerId: (playerName: string) => number;
+    setPlayerScore: (playerId: number, round: number, score: number) => void;
+  };
+
+  // enter score pocha view
+  'enterScore:pocha': {
+    playerNames: string[];
+  };
+
+  // enter score brisca view
+  'enterScore:brisca': {
+    modality: 'individual' | 'teams';
+    playerNames: string[];
+    teamNames: string[];
+    scores: number[];
+    setNextDealingPlayer(): void;
   };
 };
 
