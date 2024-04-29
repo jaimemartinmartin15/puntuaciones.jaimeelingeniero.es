a3cd7b9e7d95b95fd897e985e69abad5d651fbf5
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-04-29 19:46:56 +0200

    Adapt brisca service to new flags.

--- src/app/game-services/brisca.service.ts
+++ src/app/game-services/brisca.service.ts
@@ -4,11 +4,24 @@ import { Router } from '@angular/router';
 import { EnterPlayerNamesModel } from '../components/enter-player-names/enter-player-names.component';
 import { LOCAL_STORE_KEYS } from '../constants/local-storage-keys';
 import { ROUTING_PATHS, RoutingPath } from '../constants/routes';
-import { Player } from '../interfaces/player';
 import { Flag, FlagMapping } from './flags';
 import { GameServiceWithFlags } from './game.service';
 
-const briscaFlags = ['resumeGame:gameName', 'gameConfig:modality', 'roundInfo:gameName', 'bottomControls:newRound'] as const;
+const briscaFlags = [
+  'game:gameStartEnd',
+  'game:localStorageSave',
+  'game:rounds',
+  'resumeGame:gameName',
+  'gameConfig',
+  'gameConfig:validation',
+  'gameConfig:players',
+  'gameConfig:modality',
+  'roundInfo:gameName',
+  'roundInfo:dealingPlayer',
+  'bottomControls:newRound',
+  'brisca',
+  'enterScore:brisca',
+] as const;
 
 type BriscaFlags = (typeof briscaFlags)[number];
 
@@ -17,10 +30,7 @@ export class BriscaService implements GameServiceWithFlags<BriscaFlags> {
   private readonly modalityIndividualTeamControl = this.fb.control({ teamName: 'Jugadores', playerNames: ['', ''], dealingPlayerIndex: 0 });
   private readonly modalityTeamsTeam1Control = this.fb.control({ teamName: 'Nosotros', playerNames: ['', '', ''], dealingPlayerIndex: 0 });
   private readonly modalityTeamsTeam2Control = this.fb.control({ teamName: 'Ellos', playerNames: ['', '', ''], dealingPlayerIndex: -1 });
-
-  public playerNames: string[] = [];
-  public scores: number[] = [0, 0];
-  public teamNames: string[] = [];
+  private dealingPlayerIndex: number = 0;
 
   public constructor(private readonly fb: NonNullableFormBuilder, private readonly router: Router) {
     this.modalityFormControl = this.fb.control(this.modality);
@@ -66,7 +76,14 @@ export class BriscaService implements GameServiceWithFlags<BriscaFlags> {
     }
   }
 
+  //#region GameService
+
+  public readonly gameName = 'Brisca';
+
+  public readonly startGameRoute: RoutingPath = ROUTING_PATHS.BRISCA;
+
   public readonly flags: Flag[] = briscaFlags as any as BriscaFlags[];
+
   public hasFlagActive<K extends keyof FlagMapping>(flag: K): this is GameServiceWithFlags<K> {
     // do not allow to change the modality once the game has started (edition mode)
     if (this.router.url === `/${ROUTING_PATHS.CHANGE_CONFIG}` && flag === 'gameConfig:modality') return false;
@@ -74,21 +91,35 @@ export class BriscaService implements GameServiceWithFlags<BriscaFlags> {
     return this.flags.includes(flag);
   }
 
-  public readonly gameName = 'Brisca';
+  public isGameServiceWithFlags(flags: Flag[]) {
+    return flags.every((f) => this.hasFlagActive(f));
+  }
 
-  public allowEditTeamName = [true, true];
+  //#endregion GameService
 
-  public teamControls: FormArray<FormControl<EnterPlayerNamesModel>>; // init in the constructor
+  //#region game:gameStartEnd
+
+  public gameHasStarted(): boolean {
+    return this.scores.reduce((prev, curr) => prev + curr, 0) > 0;
+  }
+
+  public gameHasFinished(): boolean {
+    return false;
+  }
+
+  //#endregion game:gameStartEnd
+
+  //#region game:localStorageSave
 
   public saveStateToLocalStorage(): void {
     localStorage.setItem(
       LOCAL_STORE_KEYS.SETTINGS(this.gameName),
       JSON.stringify({
         modality: this.modality,
-        playerNames: this.playerNames,
         scores: this.scores,
-        dealingPlayerIndex: this.dealingPlayerIndex,
         teamNames: this.teamNames,
+        playerNames: this.playerNames,
+        dealingPlayerIndex: this.dealingPlayerIndex,
       })
     );
   }
@@ -98,10 +129,11 @@ export class BriscaService implements GameServiceWithFlags<BriscaFlags> {
     this.modality = settings.modality;
     this.modalityFormControl.setValue(this.modality);
 
-    this.playerNames = settings.playerNames;
     this.scores = settings.scores;
-    this.dealingPlayerIndex = settings.dealingPlayerIndex;
+
     this.teamNames = settings.teamNames;
+    this.playerNames = settings.playerNames;
+    this.dealingPlayerIndex = settings.dealingPlayerIndex;
 
     this.teamControls.clear();
     if (this.modality === 'individual') {
@@ -128,110 +160,139 @@ export class BriscaService implements GameServiceWithFlags<BriscaFlags> {
     }
   }
 
-  public isGameConfigCorrect(): boolean {
-    return this.teamControls.controls.every(
-      (control) => control.value.playerNames.every((p) => p.trim() !== '') && control.value.teamName.trim() !== ''
-    );
+  //#endregion game:localStorageSave
+
+  //#region game:rounds
+
+  public getNextRoundNumber(): number {
+    return this.scores.reduce((prev, curr) => prev + curr, 1);
   }
+
+  //#endregion game:rounds
+
+  //#region resumeGame:gameName
+
+  // gameName: string -> property already declared by GameService
+
+  //#endregion resumeGame:gameName
+
+  //#region gameConfig
 
   public onStartGame(): void {
     this.modality = this.modalityFormControl.value;
 
     if (this.modality === 'individual') {
-      this.playerNames = this.modalityIndividualTeamControl.value.playerNames;
-      this.scores = this.playerNames.map(() => 0);
+      this.playerNames = this.modalityIndividualTeamControl.value.playerNames.map((n) => n.trim());
       this.dealingPlayerIndex = this.modalityIndividualTeamControl.value.dealingPlayerIndex;
+      this.scores = this.playerNames.map(() => 0);
     } else if (this.modality === 'teams') {
-      const playerNamesTeam1 = this.modalityTeamsTeam1Control.value.playerNames;
-      const playerNamesTeam2 = this.modalityTeamsTeam2Control.value.playerNames;
+      const playerNamesTeam1 = this.modalityTeamsTeam1Control.value.playerNames.map((n) => n.trim());
+      const playerNamesTeam2 = this.modalityTeamsTeam2Control.value.playerNames.map((n) => n.trim());
       this.playerNames = playerNamesTeam1.map((_: string, i: number) => [playerNamesTeam1[i], playerNamesTeam2[i]]).flatMap((e) => e);
-      this.scores = [0, 0];
-      this.teamNames = [this.modalityTeamsTeam1Control.value.teamName, this.modalityTeamsTeam2Control.value.teamName];
+      this.teamNames = [this.modalityTeamsTeam1Control.value.teamName.trim(), this.modalityTeamsTeam2Control.value.teamName.trim()];
       this.dealingPlayerIndex =
         this.modalityTeamsTeam1Control.value.dealingPlayerIndex !== -1
           ? this.modalityTeamsTeam1Control.value.dealingPlayerIndex * 2
           : this.modalityTeamsTeam2Control.value.dealingPlayerIndex * 2 + 1;
+      this.scores = [0, 0];
     }
 
-    // show always the delete banner after starting a new game
-    const oneDayAgo = Date.now() - 24 * 60 * 60 * 1000;
-    localStorage.setItem(LOCAL_STORE_KEYS.BRISCA_LAST_TIME_DELETE_BANNER, JSON.stringify(Date.now() - oneDayAgo));
+    // show always the delete banner after starting a new game by deleting the local storage key
+    localStorage.removeItem(LOCAL_STORE_KEYS.BRISCA_LAST_TIME_DELETE_BANNER);
   }
 
   public onEditConfigCurrentGame(): void {
     if (this.modality === 'individual') {
       this.dealingPlayerIndex = this.modalityIndividualTeamControl.value.dealingPlayerIndex;
-      this.playerNames = this.modalityIndividualTeamControl.value.playerNames;
-      this.scores.length = this.playerNames.length;
-      for (let i = 0; i < this.scores.length; i++) {
-        if (!this.scores[i]) this.scores[i] = 0;
-      }
+
+      const newPlayersAndScores = this.modalityIndividualTeamControl.value.playerNames
+        .map((n) => n.trim())
+        .map((newName) => {
+          const existingPlayer = this.playerNames.includes(newName);
+
+          if (existingPlayer) {
+            return { name: newName, score: this.scores[this.playerNames.indexOf(newName)] };
+          }
+
+          return { name: newName, score: 0 };
+        });
+
+      this.playerNames = newPlayersAndScores.map((e) => e.name);
+      this.scores = newPlayersAndScores.map((e) => e.score);
       return;
     }
 
     // modality -> teams
-    const playerNamesTeam1 = this.modalityTeamsTeam1Control.value.playerNames;
-    const playerNamesTeam2 = this.modalityTeamsTeam2Control.value.playerNames;
+    const playerNamesTeam1 = this.modalityTeamsTeam1Control.value.playerNames.map((n) => n.trim());
+    const playerNamesTeam2 = this.modalityTeamsTeam2Control.value.playerNames.map((n) => n.trim());
     this.playerNames = playerNamesTeam1.map((_: string, i: number) => [playerNamesTeam1[i], playerNamesTeam2[i]]).flatMap((e) => e);
-    this.teamNames = [this.modalityTeamsTeam1Control.value.teamName, this.modalityTeamsTeam2Control.value.teamName];
+    this.teamNames = [this.modalityTeamsTeam1Control.value.teamName.trim(), this.modalityTeamsTeam2Control.value.teamName.trim()];
     this.dealingPlayerIndex =
       this.modalityTeamsTeam1Control.value.dealingPlayerIndex !== -1
         ? this.modalityTeamsTeam1Control.value.dealingPlayerIndex * 2
         : this.modalityTeamsTeam2Control.value.dealingPlayerIndex * 2 + 1;
   }
 
-  public readonly startGameRoute: RoutingPath = ROUTING_PATHS.BRISCA;
+  //#endregion gameConfig
 
-  public readonly enterScoreRoute: RoutingPath = ROUTING_PATHS.ENTER_SCORE_BRISCA;
+  //#region gameConfig:validation
 
-  // TODO check if started and finsied interface is ncessary and remove the dependency creating a new flag
-  public gameHasStarted(): boolean {
-    throw new Error('Method not implemented.');
-  }
-  public gameHasFinished(): boolean {
-    return false;
+  public isGameConfigCorrect(): boolean {
+    return this.teamControls.controls.every(
+      (control) => control.value.playerNames.every((p) => p.trim() !== '') && control.value.teamName.trim() !== ''
+    );
   }
 
-  public getNextRoundNumber(): number {
-    return this.scores.reduce((prev, curr) => prev + curr, 1);
-  }
+  //#endregion gameConfig:validation
+
+  //#region gameConfig:players
+
+  public allowEditTeamName = [true, true];
+
+  // init in the constructor
+  public teamControls: FormArray<FormControl<EnterPlayerNamesModel>>;
+
+  //#endregion gameConfig:players
+
+  //#region gameConfig:modality
+
+  public modality: 'individual' | 'teams' = 'teams';
+
+  // init in the constructor
+  public modalityFormControl: FormControl<'individual' | 'teams'>;
+
+  //#endregion gameConfig:modality
+
+  //#region roundInfo:gameName
+
+  // gameName: string -> property already declared by GameService
+
+  //#endregion roundInfo:gameName
+
+  //#region roundInfo:dealingPlayer
 
   public getPlayerNameThatDeals(): string {
     return this.playerNames[this.dealingPlayerIndex];
   }
 
-  // TODO check if players interface is used or the dependency with bottom control can be removed
-  private _players: Player[] = [];
+  //#endregion roundInfo:dealingPlayer
 
-  public get players(): Player[] {
-    return [...this._players];
-  }
+  //#region bottomControls:newRound
 
-  public set players(value: Player[]) {
-    this._players = value;
-  }
+  public readonly enterScoreRoute: RoutingPath = ROUTING_PATHS.ENTER_SCORE_BRISCA;
 
-  public dealingPlayerIndex: number = 0;
-  public setNextDealingPlayer(): void {
-    this.dealingPlayerIndex++;
-    if (this.dealingPlayerIndex >= this.playerNames.length) {
-      this.dealingPlayerIndex = 0;
-    }
-    if (this.modality === 'individual') {
-      const tempValue = this.modalityIndividualTeamControl.value;
-      tempValue.dealingPlayerIndex = this.dealingPlayerIndex;
-      this.modalityIndividualTeamControl.setValue(tempValue);
-    } else if (this.modality === 'teams') {
-      const tempValue1 = this.modalityTeamsTeam1Control.value;
-      tempValue1.dealingPlayerIndex = this.dealingPlayerIndex % 2 === 0 ? Math.floor(this.dealingPlayerIndex / 2) : -1;
-      this.modalityTeamsTeam1Control.setValue(tempValue1);
+  //#endregion bottomControls:newRound
 
-      const tempValue2 = this.modalityTeamsTeam2Control.value;
-      tempValue2.dealingPlayerIndex = this.dealingPlayerIndex % 2 !== 0 ? Math.floor(this.dealingPlayerIndex / 2) : -1;
-      this.modalityTeamsTeam2Control.setValue(tempValue2);
-    }
-  }
-  // this method is not part of any interface
+  //#region brisca
+
+  // modality: 'individual' | 'teams' -> property already declared by gameConfig:modality
+
+  public playerNames: string[] = [];
+
+  public teamNames: string[] = [];
+
+  public scores: number[] = [0, 0];
+
   public setPreviousDealingPlayerIndex(): void {
     this.dealingPlayerIndex--;
     if (this.dealingPlayerIndex < 0) {
@@ -252,7 +313,37 @@ export class BriscaService implements GameServiceWithFlags<BriscaFlags> {
     }
   }
 
-  // * flag -> gameConfig:modality
-  public modality: 'individual' | 'teams' = 'teams';
-  public modalityFormControl: FormControl<'individual' | 'teams'>; // init in the constructor
+  //#endregion brisca
+
+  //#region enterScore:brisca
+
+  // modality: 'individual' | 'teams' -> property already declared by gameConfig:modality
+
+  // playerNames: string[] -> property already declared by brisca
+
+  // teamNames: string[] -> property already declared by brisca
+
+  // scores: number[] -> property already declared by brisca
+
+  public setNextDealingPlayer(): void {
+    this.dealingPlayerIndex++;
+    if (this.dealingPlayerIndex >= this.playerNames.length) {
+      this.dealingPlayerIndex = 0;
+    }
+    if (this.modality === 'individual') {
+      const tempValue = this.modalityIndividualTeamControl.value;
+      tempValue.dealingPlayerIndex = this.dealingPlayerIndex;
+      this.modalityIndividualTeamControl.setValue(tempValue);
+    } else if (this.modality === 'teams') {
+      const tempValue1 = this.modalityTeamsTeam1Control.value;
+      tempValue1.dealingPlayerIndex = this.dealingPlayerIndex % 2 === 0 ? Math.floor(this.dealingPlayerIndex / 2) : -1;
+      this.modalityTeamsTeam1Control.setValue(tempValue1);
+
+      const tempValue2 = this.modalityTeamsTeam2Control.value;
+      tempValue2.dealingPlayerIndex = this.dealingPlayerIndex % 2 !== 0 ? Math.floor(this.dealingPlayerIndex / 2) : -1;
+      this.modalityTeamsTeam2Control.setValue(tempValue2);
+    }
+  }
+
+  //#endregion enterScore:brisca
 }
